4df9bfdd0fc88ccf2f04fcab219ea370a0dd5155
skyworkerxsky <pwuser@yandex.ru>
2022-11-10 13:37:08 +0500

    btn click test redirect

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-console.log('1. Loading page')
+window.onload = function() { 
 
-setTimeout(function() {
-    window.location = "mymts://action:screen/screen_id:5234264e-7618-8884-2f92-bfcbd3048e80"
-}, 5000)
+    console.log("1. Loading page")
+
+    // URL's
+    // location.assign('mymts://action:screen/screen_id:5234264e-7618-8884-2f92-bfcbd3048e80')
+    // window.location = "mymts://action:screen/screen_id:5234264e-7618-8884-2f92-bfcbd3048e80"
+
+    // window.location = "mymts://action:screen/screen_id:5234264e-7618-8884-2f92-bfcbd3048e80"
+
+    function createButton() {
+        console.log("2. Create button")
+        var btn = document.createElement("a")
+        btn.id = "btn"
+        btn.href = "mymts://action:screen/screen_id:5234264e-7618-8884-2f92-bfcbd3048e80"
+        btn.style.width = "10px"
+        btn.style.height = "10px"
+        document.body.appendChild(btn)
+    }
+
+    function btnClick() {
+        console.log("3. Click button")
+        var btn = document.getElementById("btn")
+        btn.click()
+    }
+
+    createButton()
+    btnClick()
+    
+}
