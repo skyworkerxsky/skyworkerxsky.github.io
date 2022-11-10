2454ae73bae3479de4f662b8ccd921b2eaac85f3
skyworkerxsky <pwuser@yandex.ru>
2022-11-10 14:13:03 +0500

    yellowgreen

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ window.onload = function() {
 
     console.log("1. Loading page")
 
-    // URL's
-    // location.assign('mymts://action:screen/screen_id:5234264e-7618-8884-2f92-bfcbd3048e80')
-    // window.location = "mymts://action:screen/screen_id:5234264e-7618-8884-2f92-bfcbd3048e80"
+    /*
+        URL's
+        mymts://action:screen/screen_id:5234264e-7618-8884-2f92-bfcbd3048e80
 
-    // window.location = "mymts://action:screen/screen_id:5234264e-7618-8884-2f92-bfcbd3048e80"
+        Methods:
+        location.assign("_url_")
+        location.href = "_url_"
+    */ 
 
     function createButton() {
         console.log("2. Create button")
         var btn = document.createElement("a")
         btn.id = "btn"
         btn.href = "mymts://action:screen/screen_id:5234264e-7618-8884-2f92-bfcbd3048e80"
-        btn.style.width = "10px"
-        btn.style.height = "10px"
         document.body.appendChild(btn)
     }
 
@@ -25,10 +26,9 @@ window.onload = function() {
     }
 
     createButton()
-    // btnClick()
 
     setTimeout(function() {
         btnClick()
-    }, 500)
+    }, 250)
     
 }
